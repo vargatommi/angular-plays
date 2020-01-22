b55a48ca4cc0b2c55cfd67e81dcc7a7da2863a12
vargatommi <vargatommi@gmail.com>
2020-01-22 13:21:08 +0100

    test change in the style

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  styles: [`h1 { font-family: Lato; font-style:italic; text-decoration: underline }`]
 })
 export class HelloComponent  {
   @Input() name: string;
